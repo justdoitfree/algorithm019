375074f38a85a51c42572c2550a62d3af2a4e080
justdoitfree <itiswangwang@163.com>
2020-11-22 23:31:54 +0800

    Week03_homework
    homework for week03

---
+++ Week_03/constructBinaryTree.java
@@ -0,0 +1,124 @@
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution032{
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+        Stack<TreeNode> roots = new Stack<TreeNode>();
+        int pre = 0;
+        int in = 0;
+        //先序遍历第一个值作为根节点
+        TreeNode curRoot = new TreeNode(preorder[pre]);
+        TreeNode root = curRoot;
+        roots.push(curRoot);
+        pre++;
+        //遍历前序遍历的数组
+        while (pre < preorder.length) {
+            //出现了当前节点的值和中序遍历数组的值相等，寻找是谁的右子树
+            if (curRoot.val == inorder[in]) {
+                //每次进行出栈，实现倒着遍历
+                while (!roots.isEmpty() && roots.peek().val == inorder[in]) {
+                    curRoot = roots.peek();
+                    roots.pop();
+                    in++;
+                }
+                //设为当前的右孩子
+                curRoot.right = new TreeNode(preorder[pre]);
+                //更新 curRoot
+                curRoot = curRoot.right;
+                roots.push(curRoot);
+                pre++;
+            } else {
+                //否则的话就一直作为左子树
+                curRoot.left = new TreeNode(preorder[pre]);
+                curRoot = curRoot.left;
+                roots.push(curRoot);
+                pre++;
+            }
+        }
+        return root;
+    }
+}
+
+public class constructBinaryTree {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (node == null) {
+                output += "null, ";
+                continue;
+            }
+
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] preorder = stringToIntegerArray(line);
+            line = in.readLine();
+            int[] inorder = stringToIntegerArray(line);
+
+            TreeNode ret = new Solution032().buildTree(preorder, inorder);
+
+            String out = treeNodeToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
